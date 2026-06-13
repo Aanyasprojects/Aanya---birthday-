548f979bf9a05e8d77f0c1d974f8cf4a2ee43953
Aanyasprojects <dhruviljain51666@gmail.com>
2026-06-13 10:36:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,65 @@
 
 function unlock(){
  let p=document.getElementById('password').value.trim().toLowerCase();
+
  if(p==='aanya'){
    document.getElementById('login').style.display='none';
    document.getElementById('site').style.display='block';
+
+   setTimeout(typeWriter,1000);
+
+   createLilies();
+   setInterval(createLilies,4000);
+
  }else{
    document.getElementById('error').innerText='Try again 🌸';
  }
 }
+
 const text = "Happy Birthday, Aanya 🌷🤍";
 let i = 0;
 
 function typeWriter() {
+  const element = document.getElementById("typewriter");
+
+  if (!element) return;
+
   if (i < text.length) {
-    document.getElementById("typewriter").innerHTML += text.charAt(i);
+    element.innerHTML += text.charAt(i);
     i++;
-    setTimeout(typeWriter, 100);
+    setTimeout(typeWriter,100);
   }
 }
 
-window.addEventListener("load", () => {
-  setTimeout(typeWriter, 1000);
-});
+function createLilies(){
+
+  const lily = document.createElement("div");
+
+  lily.innerHTML = "🪻";
+
+  lily.style.position = "fixed";
+  lily.style.left = Math.random()*100 + "vw";
+  lily.style.top = "-50px";
+  lily.style.fontSize = "28px";
+  lily.style.pointerEvents = "none";
+  lily.style.zIndex = "9999";
+
+  document.body.appendChild(lily);
+
+  let pos = -50;
+
+  const fall = setInterval(() => {
+
+    pos += 3;
+
+    lily.style.top = pos + "px";
+
+    if(pos > window.innerHeight){
+
+      clearInterval(fall);
+      lily.remove();
+
+    }
+
+  },20);
+}
